eed488a7911b261316c7d0432775dd5f0233a4b6
kourosh <kourosh23@hotmail.com>
2024-05-08 18:41:04 +0200

    products index + seed

--- BlazorAutoCrud/BlazorAutoCrud/Data/ApplicationDbContext.cs
+++ BlazorAutoCrud/BlazorAutoCrud/Data/ApplicationDbContext.cs
@@ -10,5 +10,17 @@ namespace BlazorAutoCrud.Data
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            List<Product> products = new();
+            for (int i = 1; i <= 10; i++)
+            {
+                products.Add(new Product { Id = i, Name = $"Name {i}", Quantity = i });
+            }
+            modelBuilder.Entity<Product>().HasData(products);
+        }
     }
 }
---
+++ BlazorAutoCrud/BlazorAutoCrud/Migrations/20240508164009_seed.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorAutoCrud.Migrations
+{
+    /// <inheritdoc />
+    public partial class seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, "Name 1", 1 },
+                    { 2, "Name 2", 2 },
+                    { 3, "Name 3", 3 },
+                    { 4, "Name 4", 4 },
+                    { 5, "Name 5", 5 },
+                    { 6, "Name 6", 6 },
+                    { 7, "Name 7", 7 },
+                    { 8, "Name 8", 8 },
+                    { 9, "Name 9", 9 },
+                    { 10, "Name 10", 10 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
