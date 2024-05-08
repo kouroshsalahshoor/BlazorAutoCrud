7f25258ab2ec15b0108fc8d30daf73fabe12eeff
kourosh <kourosh23@hotmail.com>
2024-05-08 20:02:42 +0200

    product crud

--- Shared/Models/Product.cs
+++ Shared/Models/Product.cs
@@ -1,8 +1,11 @@
-﻿namespace Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         public int Quantity { get; set; }
     }
